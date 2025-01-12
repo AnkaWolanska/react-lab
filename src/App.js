400e58c33ab8e1481eae3fcb05235ac8b27c305c
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2025-01-12 15:07:17 +0100

    shorten def function

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     return (
         <div>
             <h1>My favourite movies to watch</h1>
-            <h2>Title</h2>
+            <h2>Titles</h2>
             <ul>
                 {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
             </ul>
