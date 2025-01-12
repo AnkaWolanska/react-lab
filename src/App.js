2220befd95036c150b0a40f7161a6bccd0e24ee2
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2025-01-12 14:12:57 +0100

    with button

--- src/App.js
+++ src/App.js
@@ -3,28 +3,26 @@ import {useState} from "react";
 function App() {
     const [title, setTitle] = useState('Wall-E');
 
+    let feedback
+        if (title.length < 5){feedback='Tytuł jest za krótki';}
+            else if (title.length < 40){feedback='Tytuł jest w sam raz';}
+            else {feedback='Tytuł jest za długi';}
+
     function handleChange(event) {
         setTitle(event.target.value);
     }
-
-    let feedback
-        if (title.length < 5){feedback='Tytuł jest za krótki';}
-            else if (title.length < 30){feedback='Tytuł jest w sam raz';}
-            else {feedback='Tytuł jest za długi';}
-
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
+            <div>{feedback}</div>
             <input type="text" value={title} onChange={handleChange}/>
-            {
-    title.length > 0 && <div>{feedback}</div>
-}
+            <button type="button" onClick={() => alert(title)}> Pokaż tytuł
+            </button>
         </div>
     );
 }
-
 
 
 export default App;
