ff23a116174d6e9e5c78cb4c7a2f554a396054db
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2025-01-12 14:48:09 +0100

    with button

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import {useState} from "react";
 
 function App() {
     const [title, setTitle] = useState('Wall-E');
+    const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+];
 
     let feedback
-        if (title.length < 5){feedback='Tytuł jest za krótki';}
-            else if (title.length < 40){feedback='Tytuł jest w sam raz';}
-            else {feedback='Tytuł jest za długi';}
+        if (title.length < 5){feedback='Title is too short';}
+            else if (title.length < 40){feedback='Title is just right';}
+            else {feedback='Title is too long';}
 
     function handleChange(event) {
         setTitle(event.target.value);
     }
 
+    function handleShowMovieClick(){
+                alert(title);
+    }
+
     return (
         <div>
             <h1>My favourite movies to watch</h1>
+            <h2>Title</h2>
+            <ul>
+                {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+            </ul>
             <h2>My favourite movie for today is {title}</h2>
             {title.length > 0 && <div>{feedback}</div>}
             <input type="text" value={title} onChange={handleChange}/>
-            <button type="button" onClick={() => alert(title)}> Pokaż tytuł
+            <button type="button" onClick={handleShowMovieClick}> Show title
             </button>
         </div>
     );
