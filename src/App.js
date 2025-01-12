e9ad031271cc335aec46ea65070d91a297f40781
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2025-01-12 13:55:39 +0100

    with feedback

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import {useState} from "react";
+
 function App() {
-    let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
     }
+
+    let feedback
+        if (title.length < 5){feedback='Tytuł jest za krótki';}
+            else if (title.length < 30){feedback='Tytuł jest w sam raz';}
+            else {feedback='Tytuł jest za długi';}
+
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange}/>
+            {
+    title.length > 0 && <div>{feedback}</div>
+}
         </div>
     );
 }
+
+
 
 export default App;
